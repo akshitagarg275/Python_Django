bb6ca9215401e97bc67638dc456f7157e37aebba
akshita garg <akshitagarg@275@gmail.com>
2022-01-11 09:46:04 +0530

    day 1 basics

---
+++ 01basics.py
@@ -0,0 +1,114 @@
+'''
+interpretated
+Dynamic
+Indendation
+
+camel casing
+noOfPrducts
+discountOnAProduct
+
+snake casing
+no_of_products
+discount_on_a_product
+
+'''
+
+# a=15
+
+a="Python"
+print(type(a))
+print(id(a))
+
+
+'''
+[] -> square bracket
+()-> Paranthesis
+{ } -.curly braces
+
+'''
+
+'''
+Primitive datatypes
+
+Numbers: 
+        int, float , complex
+boolean: True/False
+strings: '',"" ,""" """
+None
+
+Non-primitive / Derived
+List-> []
+tuple -> ()
+dictionary -> {key:value}
+set -> {}
+range(start_index: end_index+1,step)
+'''
+
+'''
+Artihmetic : +, - , /(decimal quotient) , // (floor_divison integer_quotient), %(modulus,rem), **
+Relational : It returns True/False (>,<,>=,<=,==,!=)
+'=' assignment and '==' relational equality op
+
+a=2
+
+a=a+5
+
+a+=5
+
+Logical op: and (if anyone i/p is False o/p False) , or (if anyone i/p is True o/p True), not (False <--> True)
+Falsy Values -> False ,None, 0
+
+Bitwise op: &,| ,^, ~, << ,>>
+
+Membership: in
+Identity: is
+
+
+'''
+
+
+'''
+conditional: only if , if-else , if-elif ladder
+
+'''
+
+age=18
+if age>=18:
+    print("You can drive")
+else:
+    print("You cannot drive")
+
+# smallest among three
+
+a=12
+b=34
+c=56
+
+# if a<b:
+#     if a<c:
+#         print("a is smallest")
+#     else:
+#         print("c is smallest")
+# else:
+#     if b<c :
+#         print("b is smallest")
+#     else:
+#         print("c is smallest")
+
+
+'''
+loops  -> while , for
+DRY -> do not Repeat Yourself 
+while v/s for
+
+we apply for loop when we don't know no of iterations
+for loop is used in iterables
+
+'''
+# find sum of digits 12345 -> 1+2+3+4+5 
+
+
+
+
+
+
